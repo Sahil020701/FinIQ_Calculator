a021a22df4fdd992df38e5e60bf9337b55f97d66
Sahil Gobade <I24.Sahil.G@FinIQ-SP.com>
2024-01-25 10:55:21 +0530

    Fixed keyboard input and error entry in history field

--- src/app/calculator/body-calc/body-calc.component.ts
+++ src/app/calculator/body-calc/body-calc.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-body-calc',
   host:{ngSkipHydration:'true'},
@@ -13,10 +13,49 @@ import { CommonModule } from '@angular/common';
 })
 
 
+
 export class BodyCalcComponent {
   inputText: string = '';
   prevResult:string = '';
   calculationHistory: string = '';
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    let key = event.key;
+
+    
+    if (/[\d\.\+\-\*\/\(\)=]|Backspace|Enter|Delete|Escape/.test(key)) {
+      event.preventDefault();
+
+      
+      if (key === 'Enter') {
+        key = '='; 
+      }
+      if (key === "Backspace"){
+        key = 'Del'
+      }
+      if (key === "+"){
+        key = '+'
+      }
+      if (key === "-"){
+        key = '-'
+      }
+      if (key === "("){
+        key = '('
+      }
+      if (key === ")"){
+        key = ')'
+      }
+      if (key === "/"){
+        key = '/'
+      }
+      if (key === "."){
+        key = '.'
+      }
+
+      this.onButtonClick(key);
+    }
+  }
 
   updateCalculationHistory(prevResult: string , inputText:string) {
 
@@ -24,6 +63,7 @@ export class BodyCalcComponent {
     this.calculationHistory += historyEntry + "\n";
     this.inputText = '';
   }
+  
 
 //   onButtonClick(value: string) {
 //     if (value === 'C') {
@@ -69,8 +109,10 @@ export class BodyCalcComponent {
     else if(value === '='){
       try{
         let prevResult = this.inputText;
+        if(this.inputText === 'Error'){
+          this.inputText = '';
+        }
         this.inputText = eval(this.inputText.replace(/(\..*)\./g, '$1')).toString();
-
         this.updateCalculationHistory(prevResult , this.inputText);
       }
       catch (error){
